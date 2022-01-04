d554af187e04c4b7f3297942adbca7a1087e4d74
nagapure <nagapure50@gmail.com>
2022-01-04 18:55:11 +0530

    updated files

--- gs8/course/views.py
+++ gs8/course/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def learn_django(request):
+    cname= 'Django'
+    duration = '4 months'
+    seats = 10
+    course_details ={'nm':cname, 'du':seats, 'st':seats}
+    return render(request, 'course/courseone.html',course_details)
+
+def learn_python(request):
+    return render(request, 'course/coursetwo.html') 
